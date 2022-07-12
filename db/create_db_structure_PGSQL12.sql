1c8e5483014b69d823738153df4a56585efa37d3
Kostial Jozef <kostial.jozef@ifortuna.cz>
2022-07-12 14:49:18 +0200

    mastering DB structure

--- db/create_db_structure_PGSQL12.sql
+++ db/create_db_structure_PGSQL12.sql
@@ -1,74 +1,4 @@
 -- #### TABLES #####
-
--- public.tab_sj definition
-
--- Drop table
-
--- DROP TABLE tab_sj;
-
-CREATE TABLE tab_sj (
-	id_sj int4 NOT NULL,
-	sj_typ varchar(20) NULL,
-	description varchar(500) NULL,
-	interpretation varchar(150) NULL,
-	author varchar(80) NULL,
-	recorded date NULL,
-	docu_plan bool NULL,
-	docu_vertical bool NULL,
-	CONSTRAINT tab_sj_pk PRIMARY KEY (id_sj)
-);
-CREATE UNIQUE INDEX tab_sj_id_sj_idx ON public.tab_sj USING btree (id_sj);
-
-
--- public.tab_fotogram definition
-
--- Drop table
-
--- DROP TABLE tab_fotogram;
-
-CREATE TABLE tab_fotogram (
-	id_fotogram varchar(80) NOT NULL,
-	fotogram_typ varchar(60) NULL,
-	ref_sketch varchar(60) NULL,
-	notes varchar(300) NULL,
-	CONSTRAINT tab_fotogram_pk PRIMARY KEY (id_fotogram)
-);
-CREATE INDEX tab_fotogram_id_fotogram_idx ON public.tab_fotogram USING btree (id_fotogram);
-
-
--- public.tab_geopts definition
-
--- Drop table
-
--- DROP TABLE tab_geopts;
-
-CREATE TABLE tab_geopts (
-	id_pts int4 NOT NULL,
-	x float8 NULL,
-	y float8 NULL,
-	h float8 NULL,
-	code varchar(3) NULL,
-	notes varchar(200) NULL,
-	CONSTRAINT tab_geomeasuring_pk PRIMARY KEY (id_pts)
-);
-CREATE UNIQUE INDEX tab_geomeasuring_id_pts_idx ON public.tab_geopts USING btree (id_pts);
-
-
--- public.gloss_personalia definition
-
--- Drop table
-
--- DROP TABLE gloss_personalia;
-
-CREATE TABLE gloss_personalia (
-	mail varchar(80) NOT NULL,
-	"name" varchar(60) NULL,
-	surname varchar(100) NULL,
-	CONSTRAINT gloss_personalia_pk PRIMARY KEY (mail)
-);
-CREATE UNIQUE INDEX gloss_personalia_mail_idx ON public.gloss_personalia USING btree (mail);
-
-
 -- public.gloss_docu_typ definition
 
 -- Drop table
@@ -89,10 +19,56 @@ CREATE TABLE gloss_docu_typ (
 -- DROP TABLE gloss_object_type;
 
 CREATE TABLE gloss_object_type (
-	object_type varchar(100) NOT NULL,
+	object_typ varchar(100) NOT NULL,
 	description_typ varchar(200) NULL,
-	CONSTRAINT gloss_object_type_pk PRIMARY KEY (object_type)
+	CONSTRAINT gloss_object_type_pk PRIMARY KEY (object_typ)
 );
+
+
+-- public.gloss_personalia definition
+
+-- Drop table
+
+-- DROP TABLE gloss_personalia;
+
+CREATE TABLE gloss_personalia (
+	mail varchar(80) NOT NULL,
+	"name" varchar(60) NULL,
+	surname varchar(100) NULL,
+	CONSTRAINT gloss_personalia_pk PRIMARY KEY (mail)
+);
+CREATE UNIQUE INDEX gloss_personalia_mail_idx ON public.gloss_personalia USING btree (mail);
+
+
+-- public.tab_cut definition
+
+-- Drop table
+
+-- DROP TABLE tab_cut;
+
+CREATE TABLE tab_cut (
+	id_cut int4 NOT NULL,
+	description varchar(500) NULL,
+	CONSTRAINT tab_cut_pk PRIMARY KEY (id_cut)
+);
+
+
+-- public.tab_geopts definition
+
+-- Drop table
+
+-- DROP TABLE tab_geopts;
+
+CREATE TABLE tab_geopts (
+	id_pts int4 NOT NULL,
+	x float8 NULL,
+	y float8 NULL,
+	h float8 NULL,
+	code varchar(30) NULL,
+	notes varchar(200) NULL,
+	CONSTRAINT tab_geomeasuring_pk PRIMARY KEY (id_pts)
+);
+CREATE UNIQUE INDEX tab_geomeasuring_id_pts_idx ON public.tab_geopts USING btree (id_pts);
 
 
 -- public.tab_object definition
@@ -105,7 +81,7 @@ CREATE TABLE tab_object (
 	id_object int4 NOT NULL,
 	object_typ varchar(100) NULL,
 	superior_object int4 NULL DEFAULT 0,
-	notes varchar(300) NULL,
+	notes varchar(600) NULL,
 	CONSTRAINT tab_object_pk PRIMARY KEY (id_object)
 );
 
@@ -125,17 +101,57 @@ CREATE TABLE tab_polygon (
 );
 
 
--- public.tab_cut definition
+-- public.tab_sj_stratigraphy definition
 
 -- Drop table
 
--- DROP TABLE tab_cut;
+-- DROP TABLE tab_sj_stratigraphy;
 
-CREATE TABLE tab_cut (
-	id_cut int4 NOT NULL,
-	description varchar(200) NULL,
-	CONSTRAINT tab_cut_pk PRIMARY KEY (id_cut)
+CREATE TABLE tab_sj_stratigraphy (
+	id_aut serial4 NOT NULL,
+	ref_sj1 int4 NULL,
+	relation varchar(20) NULL,
+	ref_sj2 int4 NULL,
+	CONSTRAINT tab_sj_stratigraphy_pk PRIMARY KEY (id_aut)
 );
+
+
+-- public.tab_foto definition
+
+-- Drop table
+
+-- DROP TABLE tab_foto;
+
+CREATE TABLE tab_foto (
+	id_foto varchar(100) NOT NULL,
+	foto_typ varchar(60) NULL,
+	datum date NULL,
+	author varchar(100) NULL,
+	notes varchar(500) NULL,
+	CONSTRAINT tab_foto_pk PRIMARY KEY (id_foto),
+	CONSTRAINT tab_foto_fk FOREIGN KEY (author) REFERENCES gloss_personalia(mail)
+);
+
+
+-- public.tab_sj definition
+
+-- Drop table
+
+-- DROP TABLE tab_sj;
+
+CREATE TABLE tab_sj (
+	id_sj int4 NOT NULL,
+	sj_typ varchar(20) NULL,
+	description varchar(800) NULL,
+	interpretation varchar(400) NULL,
+	author varchar(100) NULL,
+	recorded date NULL,
+	docu_plan bool NULL,
+	docu_vertical bool NULL,
+	CONSTRAINT tab_sj_pk PRIMARY KEY (id_sj),
+	CONSTRAINT tab_sj_fk FOREIGN KEY (author) REFERENCES gloss_personalia(mail)
+);
+CREATE UNIQUE INDEX tab_sj_id_sj_idx ON public.tab_sj USING btree (id_sj);
 
 
 -- public.tab_sj_deposit definition
@@ -152,7 +168,8 @@ CREATE TABLE tab_sj_deposit (
 	"structure" varchar(80) NULL,
 	compactness varchar(50) NULL,
 	deposit_removed varchar(50) NULL,
-	CONSTRAINT tab_sj_deposit_pk PRIMARY KEY (id_deposit)
+	CONSTRAINT tab_sj_deposit_pk PRIMARY KEY (id_deposit),
+	CONSTRAINT tab_sj_deposit_fk FOREIGN KEY (id_deposit) REFERENCES tab_sj(id_sj)
 );
 CREATE UNIQUE INDEX tab_sj_deposit_id_deposit_idx ON public.tab_sj_deposit USING btree (id_deposit);
 
@@ -171,7 +188,8 @@ CREATE TABLE tab_sj_negativ (
 	shape_plan varchar(50) NULL,
 	shape_sides varchar(50) NULL,
 	shape_bottom varchar(50) NULL,
-	CONSTRAINT tab_sj_negativ_pk PRIMARY KEY (id_negativ)
+	CONSTRAINT tab_sj_negativ_pk PRIMARY KEY (id_negativ),
+	CONSTRAINT tab_sj_negativ_fk FOREIGN KEY (id_negativ) REFERENCES tab_sj(id_sj)
 );
 CREATE UNIQUE INDEX tab_sj_negativ_id_negativ_idx ON public.tab_sj_negativ USING btree (id_negativ);
 
@@ -191,24 +209,44 @@ CREATE TABLE tab_sj_structure (
 	length_m float8 NULL,
 	width_m float8 NULL,
 	height_m float8 NULL,
-	CONSTRAINT tab_sj_structure_pk PRIMARY KEY (id_structure)
+	CONSTRAINT tab_sj_structure_pk PRIMARY KEY (id_structure),
+	CONSTRAINT tab_sj_structure_fk FOREIGN KEY (id_structure) REFERENCES tab_sj(id_sj)
 );
 CREATE UNIQUE INDEX tab_sj_structure_id_structure_idx ON public.tab_sj_structure USING btree (id_structure);
 
 
--- public.tab_sj_stratigraphy definition
+-- public.tab_sketch definition
 
 -- Drop table
 
--- DROP TABLE tab_sj_stratigraphy;
+-- DROP TABLE tab_sketch;
 
-CREATE TABLE tab_sj_stratigraphy (
-	id_aut serial4 NOT NULL,
-	ref_sj1 int4 NULL,
-	relation varchar(20) NULL,
-	ref_sj2 int4 NULL,
-	CONSTRAINT tab_sj_stratigraphy_pk PRIMARY KEY (id_aut)
+CREATE TABLE tab_sketch (
+	id_sketch varchar(100) NOT NULL,
+	sketch_typ varchar(80) NULL,
+	author varchar(100) NULL,
+	datum date NULL,
+	notes varchar(800) NULL,
+	CONSTRAINT tab_sketch_pk PRIMARY KEY (id_sketch),
+	CONSTRAINT tab_sketch_fk FOREIGN KEY (author) REFERENCES gloss_personalia(mail)
 );
+
+
+-- public.tab_fotogram definition
+
+-- Drop table
+
+-- DROP TABLE tab_fotogram;
+
+CREATE TABLE tab_fotogram (
+	id_fotogram varchar(80) NOT NULL,
+	fotogram_typ varchar(60) NULL,
+	ref_sketch varchar(60) NULL,
+	notes varchar(800) NULL,
+	CONSTRAINT tab_fotogram_pk PRIMARY KEY (id_fotogram),
+	CONSTRAINT tab_fotogram_fk FOREIGN KEY (ref_sketch) REFERENCES tab_sketch(id_sketch)
+);
+CREATE INDEX tab_fotogram_id_fotogram_idx ON public.tab_fotogram USING btree (id_fotogram);
 
 
 -- public.tab_sack definition
@@ -220,48 +258,12 @@ CREATE TABLE tab_sj_stratigraphy (
 CREATE TABLE tab_sack (
 	id_sack int4 NOT NULL,
 	ref_sj int4 NULL,
-	"content" varchar(60) NULL,
-	datum date NULL,
-	notes varchar(300) NULL,
-	CONSTRAINT tab_sack_pk PRIMARY KEY (id_sack)
+	"content" varchar(80) NULL,
+	description varchar(600) NULL,
+	"number" int4 NULL,
+	CONSTRAINT tab_sack_pk PRIMARY KEY (id_sack),
+	CONSTRAINT tab_sack_fk FOREIGN KEY (ref_sj) REFERENCES tab_sj(id_sj)
 );
-
-
--- public.tab_sketch definition
-
--- Drop table
-
--- DROP TABLE tab_sketch;
-
-CREATE TABLE tab_sketch (
-	id_sketch integer NOT NULL,
-	sketch_typ varchar(80) NULL,
-	author varchar(100) NULL,
-	datum date NULL,
-	notes varchar(200) NULL
-);
-
-
--- public.tab_foto definition
-
--- Drop table
-
--- DROP TABLE tab_foto;
-
-CREATE TABLE tab_foto (
-	id_foto varchar(100) NOT NULL,
-	foto_typ varchar(60) NULL,
-	datum date NULL,
-	author varchar(100) NULL,
-	notes varchar(300) NULL,
-	CONSTRAINT tab_foto_pk PRIMARY KEY (id_foto)
-);
-
-
-
-
-
-
 
 
 
